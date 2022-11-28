f1dd2b3b4df06e43378b90ce45ced7d11ad37da0
shalalajun <shalalajun@naver.com>
2022-11-28 18:21:27 +0900

    addCamera
    카메라스크립트 애드

---
+++ src/Project/Camera.js
@@ -0,0 +1,36 @@
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Project from "./Project.js";
+
+export default class Camera
+{
+    constructor()
+    {
+        this.project = new Project();
+        this.sizes = this.project.sizes;
+        this.scene = this.project.scene;
+        this.canvas = this.project.canvas;
+       
+        this.setInstance();
+        this.setOrbitControls();
+    }
+
+    setInstance()
+    {
+        this.instance = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.width / this.sizes.height,
+            0.1,
+            100
+        );
+        this.instance.position.set(6, 4, 8);
+        this.scene.add(this.instance);
+    }
+
+    setOrbitControls()
+    {
+        this.controls = new OrbitControls(this.instance, this.canvas);
+        this.controls.enableDamping = true;
+    }
+}
--- src/Project/Project.js
+++ src/Project/Project.js
@@ -1,14 +1,28 @@
+import * as THREE from 'three'
 import Sizes from "./Utils/sizes.js"
 import Time from "./Utils/Time.js"
+import Camera from './Camera.js';
+
+let instance = null;
 
 export default class Project
 {
     constructor(canvas)
     {
-        window.project = this
-        this.canvas =canvas
-        this.sizes = new Sizes()
-        this.time = new Time()
+
+        if(instance)
+        {
+            return instance;
+        }
+
+        instance = this;
+
+        window.project = this;
+        this.canvas =canvas;
+        this.sizes = new Sizes();
+        this.time = new Time();
+        this.scene = new THREE.Scene();
+        this.camera = new Camera(this);
 
         this.sizes.on('resize',()=>
         {
