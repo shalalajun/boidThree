41c0d82ac86a6805e84c9ac6435505e212b6bf73
shalalajun <shalalajun@naver.com>
2022-11-28 17:34:52 +0900

    editSizes
    editSizes

--- src/Project/Utils/Sizes.js
+++ src/Project/Utils/Sizes.js
@@ -8,8 +8,8 @@ export default class Sizes
 
         window.addEventListener('resize', ()=>
         {
-            this.width = window.innerWidth()
-            this.height = window.innerHeight()
+            this.width = window.innerWidth
+            this.height = window.innerHeight
             this.pixelRatio = Math.min(window.devicePixelRatio, 2)
         })
     }
